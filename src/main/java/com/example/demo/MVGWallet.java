3243967fd2010ef4c7058d3e67d8523c5188685f
Zoomma1 <victorgerm1@hotmail.fr>
2023-12-18 16:08:52 +0100

    Made the application responsive

--- src/main/java/com/example/demo/MVGWallet.java
+++ src/main/java/com/example/demo/MVGWallet.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.effect.Light;
 import javafx.stage.Stage;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.TreeMap;
 
 /*************************************************************************************
  *This is the application class it is used to launch the application                 *
@@ -16,12 +19,16 @@ import java.io.IOException;
 
 public class MVGWallet extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-            Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-    }
+    public void start(Stage stage) throws IOException, InterruptedException, ParseException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginPage.fxml"));
+        Parent root = loader.load();
+        LoginPageController loginPageController = loader.getController();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setMinHeight(800);
+        stage.setMinWidth(900);
+        stage.show();
+        }
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/example/demo/NavBarController.java
+++ src/main/java/com/example/demo/NavBarController.java
@@ -23,8 +23,20 @@ public abstract class NavBarController {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(pageFXML));
         root = loader.load();
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+
+        double currentWidth = stage.getWidth();
+        double currentHeight = stage.getHeight();
+
+        scene = new Scene(root, currentWidth , currentHeight);
+        scene.getStylesheets().addAll(stage.getScene().getStylesheets());
+        scene.setFill(stage.getScene().getFill());
         stage.setScene(scene);
+
+        stage.setWidth(currentWidth);
+        stage.setHeight(currentHeight);
+        stage.setMinHeight(800);
+        stage.setMinWidth(900);
+
         stage.show();
     }
 
