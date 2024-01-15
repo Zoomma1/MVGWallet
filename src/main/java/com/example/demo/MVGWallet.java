396615a5adca9f9626033960588eeedf62ac737d
Mathys Haubert <mathys.haubert80@gmail.com>
2024-01-15 13:48:08 +0100

    Mh sql user (#8)
    * sql-first-part
    
    * select, selectWhere, insert
    
    * UserSQL work, can now check if user is know by dbb.

--- src/main/java/com/example/demo/MVGWallet.java
+++ src/main/java/com/example/demo/MVGWallet.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import Entity.Email.EmailUtility;
 import Entity.User;
+import Entity.UserSQL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,6 +11,13 @@ import javafx.scene.effect.Light;
 import javafx.stage.Stage;
 import org.json.simple.parser.ParseException;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.TreeMap;
 
 /*************************************************************************************
@@ -20,15 +28,22 @@ import java.util.TreeMap;
 
 public class MVGWallet extends Application {
     @Override
-    public void start(Stage stage) throws IOException, InterruptedException, ParseException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginPage.fxml"));
+    public void start(Stage stage) throws IOException, InterruptedException, SQLException, NoSuchAlgorithmException /*ParseException*/ {
+        UserSQL user = new UserSQL();
+        ArrayList<String> select = new ArrayList<>();
+        select.add("identifiant");
+        Date date = new Date();
+        Timestamp ts=new Timestamp(date.getTime());
+
+        user.checkKnowUser("mathys","123456");
+       /* FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginPage.fxml"));
         Parent root = loader.load();
         LoginPageController loginPageController = loader.getController();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setMinHeight(800);
         stage.setMinWidth(900);
-        stage.show();
+        stage.show();*/
         }
 
     public static void main(String[] args) {
